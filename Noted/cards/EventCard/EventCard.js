46fc22f825c53ae19d97f6adc5947c11ef26d9f1
asmartin <asmartin@iastate.edu>
2020-04-23 21:31:23 -0500

    I finally fixed the issue I was having, now we need to be able to inject code into the cards

--- Noted/cards/EventCard/EventCard.js
+++ Noted/cards/EventCard/EventCard.js
@@ -2,74 +2,78 @@
     EventCard.js handles data passing to the event card
     We can pass data to fill in the spans, and properly set up the event link
     https://stackoverflow.com/a/4784796
+
+    When the page runs we need to run
+
+    setTitle("string");
+    defineDate(date, date);
+    calendarLink("string", date, date);
 */
 
-class EventCard{
-    constructor(title) {
-        this.title = title;
-        var titleSpan = currentDocument.getElementById("event-title");
-        titleSpan.appendChild(currentDocument.createTextNode(title));
-        this.startDate;
-        this.endDate;
-    }
-
-    //Sets start and end date
-    //fills the spans with the corresponding data
-    //assume start and end date are already js Date() objects
-    defineDate(startDate, endDate) {
-        this.startDate = startDate;
-        this.endDate = endDate;
-
-        var dateSpan = currentDocument.getElementById("date");
-        var timeSpan = currentDocument.getElementById("time");
-6
-        dateSpan.appendChild(currentDocument.createTextNode(startDate.toDateString()));
-        var timeInterval = String(startDate.getHours()) + ":" + this.minuteMan(startDate.getMinutes()) + "-" + String(endDate.getHours()) + ":" + this.minuteMan(endDate.getMinutes());
-        timeSpan.appendChild(currentDocument.createTextNode(timeInterval));
-    }
-
-    //Give an integer of minutes
-    //if more than 1 digit, append a zero at the start
-    //return minutes as a string
-    minuteMan(minutes) {
-        if (Math.floor(minutes / 10) > 0) {//2 digits
-            return String(minutes);
-        } else {
-            return "0" + String(minutes);   //return 12:05, but just 05
-        }
-    }
-
-    //Edit the Google calendar link
-    //https://stackoverflow.com/a/21653600
-    calendarLink() {
-        //Constructing the date
-        //Using https://stackoverflow.com/a/3067896
-        //Add a function to Date()
-        Date.prototype.yyyymmdd = function () {
-            var mm = this.getMonth() + 1; // getMonth() is zero-based
-            var dd = this.getDate();
-
-            return [this.getFullYear(),
-            (mm > 9 ? '' : '0') + mm,
-            (dd > 9 ? '' : '0') + dd
-            ].join('');
-        };
-
-        //Dates must be YYYYMMDD"T"HHmmSS/YYYYMMDD"T"HHmmSS
-        var startString = this.startDate.yyyymmdd();
-        var endString = this.endDate.yyyymmdd();
-
-        var urlStr = "https://calendar.google.com/calendar/r/eventedit?" +
-            "&text=" + this.title +
-            "&dates=" + startString + "T" + this.startDate.getHours() + this.startDate.getMinutes() + "00" + "/" + endString + "T" + this.endDate.getHours() + this.endDate.getMinutes();
-        //replace the spaces with %20 for the url
-        urlStr = urlStr.replace(/ /g, "%20");
-        currentDocument.getElementById("calendarlink").href = urlStr;
-    }
-
+function setTitle(title) {
+    var titleSpan = document.getElementById("event-title");
+    titleSpan.appendChild(document.createTextNode(title));
 }
 
-//Now we run the code
-const ecard = new EventCard("school's over");
-ecard.defineDate(new Date(), new Date(2020, 11, 17, 3, 24, 0));
+//Sets start and end date
+//fills the spans with the corresponding data
+//assume start and end date are already js Date() objects
+function defineDate(startDate, endDate) {
+    var dateSpan = document.getElementById("date");
+    var timeSpan = document.getElementById("time");
 
+    dateSpan.appendChild(document.createTextNode(startDate.toDateString()));
+    var timeInterval = String(startDate.getHours()) + ":" + minuteMan(startDate) + "-" + String(endDate.getHours()) + ":" + minuteMan(endDate);
+    timeSpan.appendChild(document.createTextNode(timeInterval));
+}
+
+//Give a date
+//if more than 1 digit in minutes, append a zero at the start
+//return minutes as a string
+function minuteMan(date) {
+    var minutes = date.getMinutes();
+    if (Math.floor(minutes / 10) > 0) {//2 digits
+        return String(date.getMinutes());
+    } else {
+        return "0" + String(date.getMinutes());   //return 12:05, but just 05
+    }
+}
+
+//im too lazy so i copy pasted code sorry
+function hourMan(date) {
+    var hour = date.getHours();
+    if (Math.floor(hour / 10) > 0) {//2 digits
+        return String(date.getHours());
+    } else {
+        return "0" + String(date.getHours());   //return 12:05, but just 05
+    }
+}
+
+//Edit the Google calendar link
+//https://stackoverflow.com/a/21653600
+function calendarLink(title, startDate, endDate) {
+    //Constructing the date
+    //Using https://stackoverflow.com/a/3067896
+    //Add a function to Date()
+    Date.prototype.yyyymmdd = function () {
+        var mm = this.getMonth() + 1; // getMonth() is zero-based
+        var dd = this.getDate();
+
+        return [this.getFullYear(),
+        (mm > 9 ? '' : '0') + mm,
+        (dd > 9 ? '' : '0') + dd
+        ].join('');
+    };
+
+    //Dates must be YYYYMMDD"T"HHmmSS/YYYYMMDD"T"HHmmSS
+    var startString = startDate.yyyymmdd();
+    var endString = endDate.yyyymmdd();
+
+    var urlStr = "https://calendar.google.com/calendar/r/eventedit?" +
+        "&text=" + title +
+        "&dates=" + startString + "T" + hourMan(startDate) + minuteMan(startDate) + "00" + "/" + endString + "T" + hourMan(endDate) + minuteMan(endDate) + "00" +
+        "ctz=America%2FChicago";
+    //replace the spaces with %20 for the url
+    urlStr = urlStr.replace(/ /g, "%20");
+    document.getElementById("calendarlink").href = urlStr;
+}
