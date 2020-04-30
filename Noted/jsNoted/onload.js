299716243fa83413f1feb2fd7135dad6b6b1a393
asmartin <asmartin@iastate.edu>
2020-04-30 01:36:36 -0500

    last commit of the night

--- Noted/jsNoted/onload.js
+++ Noted/jsNoted/onload.js
@@ -8,5 +8,5 @@ init_storage();
 refreshNotes();
 
 //Load the Markdown editor
+//TODO need to consider what happens when edit note is called, will it properly grab from simplemde?
 var simplemde = new SimpleMDE({ placeholder: "Click \"save\" to save your note to your browser." });
-simplemde.value();
